45ef6848ec0fbed5368b01b401e0b13e42cab07f
Ugwuanyi Christopher Chikodili <christopherchikodili@gmail.com>
2022-12-11 04:04:54 -0800

    smile in the mirrow

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *  main - Entry Point
+ *
+ *   Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
