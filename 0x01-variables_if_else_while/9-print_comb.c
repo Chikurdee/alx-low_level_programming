4bce7cca186fdb25affb8c289efaaf06590d958c
Ugwuanyi Christopher Chikodili <christopherchikodili@gmail.com>
2022-12-11 04:35:41 -0800

    printing all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *  main - Entry Point
+ *
+ *   Return: Always 0 (success)
+ */
+int main(void)
+	{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	}
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
