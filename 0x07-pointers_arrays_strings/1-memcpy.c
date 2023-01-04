094e69c017c59c05c44c2e0a663ad08069f328f2
Ugwuanyi Christopher Chikodili <christopherchikodili@gmail.com>
2023-01-03 16:44:53 -0800

    a function that copies memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * _memcpy - fills memory with another buffer'
- * &dest: source ourse 
+ * @dest: source string
  * @src: string for filling
- * @n; lenght of bugger.
+ * @n: lenght of buffer.
+ * Return: new string
  */
-char *_memecyp(char ""dest, char *src, unsigned int 0)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-	
+
 	while (i < n)
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	return (dest;
-)
+	return (dest);
+}
