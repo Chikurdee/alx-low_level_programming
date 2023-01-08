8e569bf6a5f8536e523964c674975eeef4698d51
Ugwuanyi Christopher Chikodili <christopherchikodili@gmail.com>
2023-01-07 20:33:04 -0800

    advanced task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _lenght - checks the lenght of a string
+ * @s: is the string
+ * Return: return te lenght of the srting
+ */
+int _lenght(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _lenght(s + 1));
+}
+/**
+ * checkp - checks if the string is palindrome
+ * @i: is the index
+ * @lg: is the lenght of the string
+ * @s: is the string
+ * Return: 1 if it is palindrome or 0 if not
+ */
+int checkp(int i, int lg, chr *s)
+{
+	if (lg > 0)
+	{
+		if (s[i] == s[lg])
+		{
+			return (checkp(i + 1, lg - 1, s));
+		}
+		else if (s[i] != s[lg])
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindroe
+ * @s: is a string
+ * Return: return 1 if the string is a palindrome or 0 if otherwise
+ */
+int is_palindrome(char *s)
+{
+	return (checkp(0, _lenght(s) - 1, s));
+}
